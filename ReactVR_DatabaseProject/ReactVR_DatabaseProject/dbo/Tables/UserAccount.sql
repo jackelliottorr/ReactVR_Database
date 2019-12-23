7f9187f2fd193b2c97023296730f7405894396a6
Vandalaz <jackelliottorr@gmail.com>
2019-12-23 20:05:03 +0000

    Closes  #1
    Created the project, set up a table & a local publish profile.

---
+++ ReactVR_DatabaseProject/ReactVR_DatabaseProject/dbo/Tables/UserAccount.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserAccount]
+(
+	[UserAccountId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(128) NULL, 
+    [EmailAddress] NVARCHAR(128) NULL, 
+    [Password] NVARCHAR(256) NULL, 
+    [IsDelete] BIT NOT NULL DEFAULT 0
+)
