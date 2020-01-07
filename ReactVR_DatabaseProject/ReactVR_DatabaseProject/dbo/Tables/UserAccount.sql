e15882f41b69bd512def35cee4efb7be2e28d2ce
Vandalaz <jackelliottorr@gmail.com>
2020-01-07 01:15:25 +0000

    Table changes
    Adding CreatedDate to UserAccount

--- ReactVR_DatabaseProject/ReactVR_DatabaseProject/dbo/Tables/UserAccount.sql
+++ ReactVR_DatabaseProject/ReactVR_DatabaseProject/dbo/Tables/UserAccount.sql
@@ -4,5 +4,6 @@
     [Name] NVARCHAR(128) NULL, 
     [EmailAddress] NVARCHAR(128) NULL, 
     [Password] NVARCHAR(256) NULL, 
-    [IsDelete] BIT NOT NULL DEFAULT 0
+    [CreatedDate] DATETIME NOT NULL,
+    [IsDeleted] BIT NOT NULL DEFAULT 0
 )
