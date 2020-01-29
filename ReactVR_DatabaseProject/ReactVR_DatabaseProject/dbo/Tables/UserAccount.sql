2378f548145da4232d48096ffb151d671d506716
Vandalaz <jackelliottorr@gmail.com>
2020-01-29 22:47:34 +0000

    Update UserAccount.sql to add Salt & Hash

--- ReactVR_DatabaseProject/ReactVR_DatabaseProject/dbo/Tables/UserAccount.sql
+++ ReactVR_DatabaseProject/ReactVR_DatabaseProject/dbo/Tables/UserAccount.sql
@@ -2,8 +2,9 @@
 (
 	[UserAccountId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(), 
     [Name] NVARCHAR(128) NULL, 
-    [EmailAddress] NVARCHAR(128) NULL, 
-    [Password] NVARCHAR(256) NULL, 
+    [EmailAddress] NVARCHAR(128) NOT NULL, 
+    [Salt] NVARCHAR(128) NOT NULL, 
+    [Hash] NVARCHAR(128) NOT NULL, 
     [CreatedDate] DATETIME NOT NULL DEFAULT GETDATE(),
     [IsDeleted] BIT NOT NULL DEFAULT 0
 )
